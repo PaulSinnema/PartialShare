f1b87dbd4e754703c179eaa17a366bb81c5f8d5b
Paul Sinnema <paul@sinnema.ch>
2022-02-28 16:32:32 +0100

    First check in

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Interop;
+
+namespace PartialShare
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            ToolWindow tw = new ToolWindow();
+            tw.Show();
+
+            InitializeComponent();
+        }
+
+        protected override void OnSourceInitialized(EventArgs e)
+        {
+            base.OnSourceInitialized(e);
+
+            // Make entire window and everything in it transparent to the Mouse
+
+            // Make the button "visible" to the Mouse
+            // var buttonHwndSource = (HwndSource)HwndSource.FromVisual(btn);
+            // var buttonHwnd = buttonHwndSource.Handle;
+            // WindowsServices.SetWindowExNotTransparent(buttonHwnd);
+        }
+    }
+}
